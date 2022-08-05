d674670473d471505d5a75f53c3a6521950093d4
sandrageovany <sandra.geovany@gmail.com>
2022-08-05 13:20:48 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from distutils.log import debug
+from flask import Flask
+
+app= Flask(__name__)
+
+# Homepage Route
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
+if __name__=="__main__":
+    app.run(debug==True)
